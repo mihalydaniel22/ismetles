747e1b5596be0dc0a6c2bc6502f5af0c6bbc9e60
mihalydaniel22 <rikkiardo3@gmail.com>
2020-09-08 11:22:09 +0200

    A felhasználó választ

--- ismetles/Program.cs
+++ ismetles/Program.cs
@@ -14,6 +14,11 @@ namespace ismetles
             string[] lehetoseg = new string[] { "Kő", "Papír", "Olló" };
             int gepValasz = vel.Next(0, 3);
             Console.WriteLine("Gép választása: {0}", lehetoseg[gepValasz]);
+            int jatekosvalasz;
+            Console.WriteLine("Kő {0}, Papír {1}, Olló {2}");
+            Console.Write("Válassz: ");
+            jatekosvalasz = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Játékos választása: {0}", lehetoseg[jatekosvalasz]);
             Console.ReadKey();
         }
     }
